196673d9f134070a863e5678307d466e96d6859a
Akira Ohgaki <akiraohgaki@gmail.com>
2022-11-16 15:10:50 +0900

    Add logs

--- server.ts
+++ server.ts
@@ -7,12 +7,22 @@ let processing = false;
 
 const runWhisper = async (model: string, language: string) => {
   processing = true;
+  const startMessage = `Transcribe started at ${(new Date()).toUTCString()}`;
+  console.log(startMessage);
+
   await $`whisper /tmp/audio --model ${model} --language ${language} --output_dir /tmp`;
+
   processing = false;
+  const finishMessage = `Transcribe finished at ${(new Date()).toUTCString()}`;
+  console.log(finishMessage);
+
+  await Deno.writeTextFile('/tmp/audio.txt', `\n\n--\n\n${startMessage}\n${finishMessage}`, { append: true });
 };
 
 await serve(async (request) => {
   try {
+    console.log(`${request.method} ${request.url}`);
+
     const url = new URL(request.url);
 
     if (request.method === 'GET' && url.pathname === '/health') {
@@ -22,11 +32,13 @@ await serve(async (request) => {
       });
     } else if (request.method === 'GET' && url.pathname === '/transcript') {
       let data = '';
+
       try {
         data = await Deno.readTextFile('/tmp/audio.txt');
       } catch {
         void 0;
       }
+
       return new Response(data, {
         status: 200,
         headers: { 'Content-Type': 'text/plain' },
@@ -35,12 +47,15 @@ await serve(async (request) => {
       if (!processing) {
         const formData = await request.formData();
         const data = await new Response(formData.get('audio')).arrayBuffer();
+
         await Deno.writeFile('/tmp/audio', new Uint8Array(data));
+
         try {
           await Deno.remove('/tmp/audio.txt');
         } catch {
           void 0;
         }
+
         runWhisper(url.searchParams.get('model') || 'base', url.searchParams.get('language') || 'en');
       }
       return new Response('Now processing...', {
